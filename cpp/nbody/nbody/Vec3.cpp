f9f6615e3bc895ae8a291886ab79d5436fd758ac
Phillip Jenks <doomacle@gmail.com>
2016-06-14 15:25:30 -0700

    More vector utilities

--- cpp/nbody/nbody/Vec3.cpp
+++ cpp/nbody/nbody/Vec3.cpp
@@ -27,11 +27,42 @@ double Vec3::lengthSquare() const {
 	return x * x + y * y + z * z;
 }
 
+// Normalize the vector
+void Vec3::normalize() {
+	
+	double length = this->length();
+	if (length > 0) {
+		x /= length;
+		y /= length;
+		z /= length;
+	}
+}
+
 Vec3& Vec3::operator+=(const Vec3& other) {
 	
 	x += other.x;
 	y += other.y;
 	z += other.z;
+
+	return *this;
+}
+
+Vec3& Vec3::operator*=(double scale) {
+
+	x *= scale;
+	y *= scale;
+	z *= scale;
+
+	return *this;
+}
+
+Vec3& Vec3::operator/=(double scale) {
+
+	if (scale != 0) {
+		x /= scale;
+		y /= scale;
+		z /= scale;
+	}
 
 	return *this;
 }
@@ -43,10 +74,28 @@ Vec3 operator+(const Vec3& lhs, const Vec3& rhs) {
 Vec3 operator*(double scale, const Vec3& vec) {
 	return Vec3(scale * vec.x, scale * vec.y, scale * vec.z);
 }
+Vec3 operator*(const Vec3& vec, double scale) {
+	return scale * vec;
+}
+
+// Return 0 vector if attempt to divide by zero
+Vec3 operator/(const Vec3& vec, double scale) {
+	
+	Vec3 ret;
+	if (scale != 0) {
+		ret = vec;
+		ret /= scale;
+	}
+	return ret;
+}
 
 Vec3 operator-(const Vec3& vec) {
 	return Vec3(-vec.x, -vec.y, -vec.z);
 }
 Vec3 operator-(const Vec3& lhs, const Vec3& rhs) {
 	return lhs + (-rhs);
+}
+
+std::ostream& operator<<(std::ostream& out, const Vec3& vec) {
+	return out << "(" << vec.x << ", " << vec.y << ", " << vec.z << ")";
 }
--- cpp/nbody/nbody/Vec3.h
+++ cpp/nbody/nbody/Vec3.h
@@ -2,6 +2,8 @@
 #ifndef __VEC3_H_
 #define __VEC3_H_
 
+#include <iostream>
+
 class Vec3
 {
 public:
@@ -24,13 +26,22 @@ public:
 	double length() const;
 	double lengthSquare() const;
 
+	// Normalize our vector
+	void normalize();
+
 	Vec3& operator+=(const Vec3& other);
+	Vec3& operator*=(double scale);
+	Vec3& operator/=(double scale);
 };
 
 // Utility operator overloading
 Vec3 operator+(const Vec3& lhs, const Vec3& rhs);
 Vec3 operator*(double scale, const Vec3& vec);
+Vec3 operator*(const Vec3& vec, double scale);
+Vec3 operator/(const Vec3& vec, double scale);
 Vec3 operator-(const Vec3& vec);
 Vec3 operator-(const Vec3& lhs, const Vec3& rhs);
+
+std::ostream& operator<<(std::ostream& out, const Vec3& vec);
 
 #endif
