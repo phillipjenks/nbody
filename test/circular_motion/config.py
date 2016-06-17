c63d21a744787479b78ceb1b7d65832c2fa0844e
Phillip Jenks <doomacle@gmail.com>
2016-06-17 01:09:48 -0700

    Added a circular motion test problem

---
+++ test/circular_motion/config.py
@@ -0,0 +1,16 @@
+import os, sys
+sys.path.append(os.path.join(os.getcwd(), '..', '..'))
+
+from problem_builder import ConfigFileBuilder
+
+class Config(ConfigFileBuilder):
+
+	def __init__(self, problem):
+		ConfigFileBuilder.__init__(self, problem)
+
+		self.config['TimeStep'] = 0.001
+		self.config['OutputFrequency'] = 0.01
+		self.config['TotalTime'] = 2
+		self.config['SofteningParam'] = 0.0002
+		self.config['GravityStrength'] = 1
+		self.config['DebugOutput'] = 0
---
+++ test/circular_motion/problem.py
@@ -0,0 +1,23 @@
+import os, sys, math
+sys.path.append(os.path.join(os.getcwd(), '..', '..'))
+
+from problem_builder import BodyBuilder
+from problem_builder import Body
+
+class Problem(BodyBuilder):
+
+	def __init__(self):
+		BodyBuilder.__init__(self)
+
+	def build_bodies(self):
+
+		self.bodies.append(Body(1, [0, 0, 0], [0, 0, 0]))
+
+		r = 10
+
+		# GM / r^2 = v^2 / r
+		# => v = sqrt(GM / r)
+
+		self.bodies.append(Body(0.00001, [r, 0, 0], [0, math.sqrt(1 / float(r)), 0]))
+
+		return self.bodies		
