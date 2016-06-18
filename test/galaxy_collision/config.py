fe8ee69c78c4b5ecb08d2a8b86f057d5b8ed98ab
Phillip Jenks <doomacle@gmail.com>
2016-06-17 22:04:45 -0700

    Updated galaxy collision problem

--- test/galaxy_collision/config.py
+++ test/galaxy_collision/config.py
@@ -7,10 +7,10 @@ class Config(ConfigFileBuilder):
 		ConfigFileBuilder.__init__(self, problem)
 
 		self.config['TimeStep'] = 0.0001
-		self.config['OutputFrequency'] = 0.001
-		self.config['TotalTime'] = .05
+		self.config['OutputFrequency'] = 0.01
+		self.config['TotalTime'] = 5
 		self.config['SofteningParam'] = 0.0002
 		self.config['GravityStrength'] = 1
 		self.config['DebugOutput'] = 0
 
-		self.visualization_config['custom_lim'] = 80
+		self.visualization_config['custom_lim'] = 200
