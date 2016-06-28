ead44324f25be6d8fd897193fd873cf7683a426c
Phillip Jenks <phillip.jenks@gmail.com>
2016-06-28 16:16:07 -0700

    Better body distribution across threads

--- cpp/nbody/nbody/SimBodyManager.cpp
+++ cpp/nbody/nbody/SimBodyManager.cpp
@@ -26,14 +26,21 @@ void SimBodyManager::runTimeStep(double dt) {
 
 		std::vector<std::thread> threads;
 		unsigned int elemsPerThread = bodies.size() / NUM_THREADS;
-		for (unsigned int th = 0; th < NUM_THREADS + 1; ++th) {
-			std::size_t start = th * elemsPerThread;
-			std::size_t end = std::min(start + elemsPerThread - 1, bodies.size() - 1);
+		unsigned int leftover = bodies.size() % NUM_THREADS;
+		std::size_t start = 0;
 
-			if (start < bodies.size()) {
-				//std::cout << "Thread " << th << " got elems " << start << " - " << end << " for " << bodies.size() << " bodies " << elemsPerThread << std::endl;
-				threads.push_back(std::thread(runStep, start, end, &bodies, dt));
+		for (unsigned int th = 0; th < NUM_THREADS; ++th) {
+			
+			std::size_t end = start + elemsPerThread - 1;
+			if (leftover > 0) {
+				++end;
+				--leftover;
 			}
+
+			// std::cout << "Thread " << th << " got elems " << start << " - " << end << " for " << bodies.size() << " bodies " << elemsPerThread << std::endl;
+			threads.push_back(std::thread(runStep, start, end, &bodies, dt));
+
+			start = end + 1;
 		}
 
 		for (auto& th : threads) {
