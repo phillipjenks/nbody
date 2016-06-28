7788b310537e4b09ba328db07a96f203fed3f3ae
Phillip Jenks <phillip.jenks@gmail.com>
2016-06-28 14:59:50 -0700

    Added some comments to the TimedInterval class

--- cpp/nbody/nbody/TimedInterval.cpp
+++ cpp/nbody/nbody/TimedInterval.cpp
@@ -3,13 +3,14 @@
 
 #include "TimeProfiler.h"
 
+// Constructor that starts a timed interval
 TimedInterval::TimedInterval(const std::string& name)
 	: name(name)
 {
 	TimeProfiler::GetProfiler().startInterval(name);
 }
 
-
+// Destructor that stops a timed interval
 TimedInterval::~TimedInterval() {
 
 	TimeProfiler::GetProfiler().stopInterval(name);
--- cpp/nbody/nbody/TimedInterval.h
+++ cpp/nbody/nbody/TimedInterval.h
@@ -1,4 +1,10 @@
 
+/*
+
+	Utility class for creating a timed interval
+
+*/
+
 #ifndef __TIMED_INTERVAL_H_
 #define __TIMED_INTERVAL_H_
 
@@ -9,7 +15,11 @@
 class TimedInterval
 {
 public:
+
+	// Constructor will use the time profiler singleton to start a timed interval
 	TimedInterval(const std::string&);
+
+	// Destructor will use the time profiler singleton to stop a timed interval
 	~TimedInterval();
 
 private:
